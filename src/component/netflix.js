305470e7becfe3375955f0f4b20814a6dd81c879
iicaie <erickohyewweng@gmail.com>
2020-12-05 15:20:29 +0800

    test

---
+++ src/component/netflix.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Netflix extends Component {
+    render(){
+        return (
+            <div>
+testing
+            </div>
+        )
+    }
+}
